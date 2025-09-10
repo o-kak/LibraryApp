7cb184d051d69e2d6784d6a0b37d146d6f941d14
evelson <euroset882605@gmail/com>
2025-09-10 22:02:18 +0700

    добавлены сущности и основная бизнес логика

---
+++ Model/LibraryManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    internal class LibraryManager
+    {
+        public List<Reader> Readers;
+        public List<Book> Books;
+
+        private Random random;
+        public LibraryManager()
+        {
+            Readers = new List<Reader>();
+            Books = new List<Book>();
+            random = new Random();
+        }
+
+        public void AddReader(string name, string address)
+        {
+            int id = Readers.Count + 1;
+            Reader reader = new Reader(name, address, id);
+            Readers.Add(reader);
+        }
+
+        public void AddBook(string title, string author, string genre)
+        {
+            Book book = new Book(title, author, genre);
+            Books.Add(book);
+        }
+
+        public void DeleteReader(Reader reader)
+        {
+            Readers.Remove(reader);
+        }
+
+        public void DeleteBook(Book book)
+        {
+            Books.Remove(book);
+        }
+    }
+}
---
+++ Model/Reader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Reader
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ID { get; set; }
+        public List<Book> BooksBorrowed { get; set; } = new List<Book>();
+
+        public Reader(string name, string address, int id)
+        {
+            Name = name;
+            Address = address;
+            ID = id;
+        }
+    }
+}
