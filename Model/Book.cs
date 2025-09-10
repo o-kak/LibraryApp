506d38af922718bae405ec6234e0100b0fc3313c
evelson <euroset882605@gmail/com>
2025-09-10 22:02:18 +0700

    добавлены сущности и основная бизнес логика

---
+++ Model/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Genre { get; set; }
+
+        public Book(string title, string author, string genre)
+        {
+            Title = title;
+            Author = author;
+            Genre = genre;
+        }
+    }
+}
