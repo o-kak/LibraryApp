ad16ea426eee61ff7a5231396fef316f09063a29
evelson <euroset882605@gmail/com>
2025-09-11 21:29:21 +0700

    прописана логика сущностей и основные методы бизнес логики

--- Model/Book.cs
+++ Model/Book.cs
@@ -11,12 +11,20 @@ namespace Model
         public string Title { get; set; }
         public string Author { get; set; }
         public string Genre { get; set; }
+        internal bool IsAvailable { get; set; }
 
         public Book(string title, string author, string genre)
         {
             Title = title;
             Author = author;
             Genre = genre;
+            IsAvailable = true;
         }
+
+        /// <summary>
+        /// изменение статуса(доступна ли книга в фонде библотеки)
+        /// </summary>
+        /// <param name="newStatus">новый статус</param>
+        public void UpdateAvailability(bool newStatus) => IsAvailable = newStatus;
     }
 }
--- Model/Reader.cs
+++ Model/Reader.cs
@@ -19,5 +19,37 @@ namespace Model
             Address = address;
             ID = id;
         }
+
+        /// <summary>
+        /// изменение имени
+        /// </summary>
+        /// <param name="name">новое имя</param>
+        public void UpdateName(string name) => Name = name;
+
+        /// <summary>
+        /// изменение адреса
+        /// </summary>
+        /// <param name="address">новый адрес</param>
+        public void UpdateAddress(string address) => Address = address;
+
+        /// <summary>
+        /// взять книгу из библиотеки - добавляет книгу в коллекцию BooksBorrowed
+        /// </summary>
+        /// <param name="book">взятая книга</param>
+        public void BorrowBook(Book book) => BooksBorrowed.Add(book);
+        
+        /// <summary>
+        /// вернуть книгу - убирает книгу из коллекции, если она там есть
+        /// </summary>
+        /// <param name="book">выбранная книга</param>
+        public bool ReturnBook(Book book)
+        {
+            if (BooksBorrowed.Contains(book))
+            {
+                BooksBorrowed.Remove(book);
+                return true;
+            }
+            return false;
+        }
     }
 }
