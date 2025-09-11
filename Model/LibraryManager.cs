b5b91ad1972b912d10b76d4ba0417db3e670c747
evelson <euroset882605@gmail/com>
2025-09-11 21:29:21 +0700

    прописана логика сущностей и основные методы бизнес логики

--- Model/LibraryManager.cs
+++ Model/LibraryManager.cs
@@ -8,35 +8,60 @@ namespace Model
 {
     internal class LibraryManager
     {
-        public List<Reader> Readers;
-        public List<Book> Books;
+        private readonly List<Reader> _readers = new List<Reader>();
+        private readonly List<Book> _books = new List<Book>();
+
+        public IEnumerable<Book> Books => _books;
+        public IEnumerable<Reader> Readers => _readers;
+
         public LibraryManager()
         {
-            Readers = new List<Reader>();
-            Books = new List<Book>();
+            _readers = new List<Reader>();
+            _books = new List<Book>();
         }
 
         public void AddReader(string name, string address)
         {
-            int id = Readers.Count + 1;
+            int id = _readers.Count + 1;
             Reader reader = new Reader(name, address, id);
-            Readers.Add(reader);
+            _readers.Add(reader);
         }
 
         public void AddBook(string title, string author, string genre)
         {
             Book book = new Book(title, author, genre);
-            Books.Add(book);
+            _books.Add(book);
         }
 
-        public void DeleteReader(Reader reader)
+        public void DeleteReader(Reader reader) => _readers.Remove(reader);
+
+        public void DeleteBook(Book book) => _books.Remove(book);
+
+        public void GiveBook(Book book, Reader reader)
         {
-            Readers.Remove(reader);
+            if (!book.IsAvailable)
+                throw new InvalidOperationException("Этой книги нет в фонде!");
+            
+            reader.BorrowBook(book);
+            book.UpdateAvailability(false);
         }
 
-        public void DeleteBook(Book book)
+        public void ReturnBook(Book book, Reader reader)
         {
-            Books.Remove(book);
+            if (!reader.ReturnBook(book))
+                throw new InvalidOperationException("У читателя нет этой книги!");
+
+            book.UpdateAvailability(true);
+        }
+
+        public IEnumerable<Book> GetAvailableBooks()
+        {
+            return _books.Where(book => book.IsAvailable);
+        }
+
+        public IEnumerable<Book> GetBorrowedBooks()
+        {
+            return _books.Where(book =>  !book.IsAvailable);
         }
     }
 }
