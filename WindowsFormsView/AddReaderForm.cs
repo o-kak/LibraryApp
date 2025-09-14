2ce06f87f4c3869a9bc5bcb7dc78c678d6923d41
SunnFloww <butenkovarvara690@gmail.com>
2025-09-14 21:18:25 +0700

    Создание новых элементов, создание доп. окна, взаимодействие между окнами, таблицы

---
+++ WindowsFormsView/AddReaderForm.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model;
+
+namespace WindowsFormsView
+{
+    public partial class AddReaderForm : Form
+    {
+        LibraryManager libraryManager = new LibraryManager();
+        public AddReaderForm()
+        {
+            InitializeComponent();
+
+
+            // Пример добавления книг
+            libraryManager.AddBook("1984", "George Orwell", "Dystopian");
+            libraryManager.AddBook ("Brave New World", "Aldous Huxley", "Science Fiction");
+            libraryManager.AddBook("Fahrenheit 451", "Ray Bradbury", "Dystopian");
+
+            FillBorrowedBooksCheckedListBox();
+        }
+
+        private void FillBorrowedBooksCheckedListBox()
+        {
+            BorrowedBooksCheckedListBox.Items.Clear(); // Очистка текущих элементов
+
+            foreach (var book in libraryManager.Books) // Предполагается, что _libraryManager инициализирован
+            {
+                BorrowedBooksCheckedListBox.Items.Add(book.Title); // Добавление названия книги
+            }
+        } 
+
+        private void SaveReaderButton_Click(object sender, EventArgs e)
+        {
+            string readerName = ReaderNamTextBox.Text;
+            string readerAdress = ReaderAdressTextBox.Text;
+
+            libraryManager.AddReader(readerName, readerAdress);
+            Reader currentReader = libraryManager.Readers.Last();
+            if (BorrowedBooksCheckedListBox.CheckedItems.Count > 0) 
+            {
+                foreach (var item in BorrowedBooksCheckedListBox.CheckedItems) 
+                {
+                    Book selectedBook = item as Book;
+                    if (selectedBook != null) 
+                    {
+                        try 
+                        {
+                            libraryManager.GiveBook(selectedBook, currentReader);
+                        }
+
+                        catch(InvalidProgramException ex) 
+                        {
+                            MessageBox.Show(ex.Message);   
+                        }
+                    }
+                }
+            }
+            Form1 mainForm = Application.OpenForms.OfType<Form1>().FirstOrDefault();
+            if (mainForm != null)
+            {
+                mainForm.UpdateReaderListView(currentReader);
+            }
+        }  
+    }
+}
--- WindowsFormsView/Form1.Designer.cs
+++ WindowsFormsView/Form1.Designer.cs
@@ -28,85 +28,178 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.listView2 = new System.Windows.Forms.ListView();
+            this.AddReaderButton = new System.Windows.Forms.Button();
+            this.AddBookButton = new System.Windows.Forms.Button();
+            this.ReaderListView = new System.Windows.Forms.ListView();
+            this.BookListView = new System.Windows.Forms.ListView();
             this.helpProvider1 = new System.Windows.Forms.HelpProvider();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.button3 = new System.Windows.Forms.Button();
+            this.DeleteReaderButton = new System.Windows.Forms.Button();
+            this.DeleteBookButton = new System.Windows.Forms.Button();
+            this.UpdateButton = new System.Windows.Forms.Button();
+            this.ReaderName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ReaderAdress = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ReaderID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ReaderBooks = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BookName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BookAuther = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BookGenre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.BookIsAvailable = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
-            // button1
+            // AddReaderButton
             // 
-            this.button1.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(40, 49);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(239, 102);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Добавить читателя";
-            this.button1.UseVisualStyleBackColor = true;
+            this.AddReaderButton.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddReaderButton.Location = new System.Drawing.Point(12, 21);
+            this.AddReaderButton.Name = "AddReaderButton";
+            this.AddReaderButton.Size = new System.Drawing.Size(250, 86);
+            this.AddReaderButton.TabIndex = 0;
+            this.AddReaderButton.Text = "Добавить читателя";
+            this.AddReaderButton.UseVisualStyleBackColor = true;
+            this.AddReaderButton.Click += new System.EventHandler(this.AddReaderButton_Click);
             // 
-            // button2
+            // AddBookButton
             // 
-            this.button2.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(716, 52);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(239, 102);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "       Добавить        книгу";
-            this.button2.UseVisualStyleBackColor = true;
+            this.AddBookButton.Font = new System.Drawing.Font("Elephant", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddBookButton.Location = new System.Drawing.Point(716, 21);
+            this.AddBookButton.Name = "AddBookButton";
+            this.AddBookButton.Size = new System.Drawing.Size(250, 86);
+            this.AddBookButton.TabIndex = 3;
+            this.AddBookButton.Text = "       Добавить        книгу";
+            this.AddBookButton.UseVisualStyleBackColor = true;
+            this.AddBookButton.Click += new System.EventHandler(this.AddBookButton_Click);
             // 
-            // listView1
+            // ReaderListView
             // 
-            this.listView1.Enabled = false;
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(40, 192);
-            this.listView1.MultiSelect = false;
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(572, 555);
-            this.listView1.TabIndex = 4;
-            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.ReaderListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ReaderName,
+            this.ReaderAdress,
+            this.ReaderID,
+            this.ReaderBooks});
+            this.ReaderListView.FullRowSelect = true;
+            this.ReaderListView.GridLines = true;
+            this.ReaderListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.ReaderListView.HideSelection = false;
+            this.ReaderListView.LabelEdit = true;
+            this.ReaderListView.Location = new System.Drawing.Point(12, 227);
+            this.ReaderListView.MultiSelect = false;
+            this.ReaderListView.Name = "ReaderListView";
+            this.ReaderListView.Size = new System.Drawing.Size(672, 527);
+            this.ReaderListView.TabIndex = 4;
+            this.ReaderListView.UseCompatibleStateImageBehavior = false;
+            this.ReaderListView.View = System.Windows.Forms.View.Details;
             // 
-            // listView2
+            // BookListView
             // 
-            this.listView2.HideSelection = false;
-            this.listView2.Location = new System.Drawing.Point(716, 192);
-            this.listView2.Name = "listView2";
-            this.listView2.Size = new System.Drawing.Size(572, 555);
-            this.listView2.TabIndex = 5;
-            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.BookListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.BookName,
+            this.BookAuther,
+            this.BookGenre,
+            this.BookIsAvailable});
+            this.BookListView.FullRowSelect = true;
+            this.BookListView.GridLines = true;
+            this.BookListView.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.BookListView.HideSelection = false;
+            this.BookListView.LabelEdit = true;
+            this.BookListView.Location = new System.Drawing.Point(716, 227);
+            this.BookListView.Name = "BookListView";
+            this.BookListView.Size = new System.Drawing.Size(616, 527);
+            this.BookListView.TabIndex = 5;
+            this.BookListView.UseCompatibleStateImageBehavior = false;
+            this.BookListView.View = System.Windows.Forms.View.Details;
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(961, 70);
+            this.checkBox1.Location = new System.Drawing.Point(972, 128);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(160, 24);
             this.checkBox1.TabIndex = 6;
             this.checkBox1.Text = "Книга в наличии";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // DeleteReaderButton
             // 
-            this.button3.Location = new System.Drawing.Point(370, 52);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(242, 102);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.DeleteReaderButton.Location = new System.Drawing.Point(12, 113);
+            this.DeleteReaderButton.Name = "DeleteReaderButton";
+            this.DeleteReaderButton.Size = new System.Drawing.Size(250, 86);
+            this.DeleteReaderButton.TabIndex = 7;
+            this.DeleteReaderButton.Text = "Удалить пользователя";
+            this.DeleteReaderButton.UseVisualStyleBackColor = true;
+            this.DeleteReaderButton.Click += new System.EventHandler(this.DeleteReaderButton_Click);
+            // 
+            // DeleteBookButton
+            // 
+            this.DeleteBookButton.Location = new System.Drawing.Point(716, 113);
+            this.DeleteBookButton.Name = "DeleteBookButton";
+            this.DeleteBookButton.Size = new System.Drawing.Size(250, 86);
+            this.DeleteBookButton.TabIndex = 8;
+            this.DeleteBookButton.Text = "Удалить книгу";
+            this.DeleteBookButton.UseVisualStyleBackColor = true;
+            // 
+            // UpdateButton
+            // 
+            this.UpdateButton.Location = new System.Drawing.Point(972, 158);
+            this.UpdateButton.Name = "UpdateButton";
+            this.UpdateButton.Size = new System.Drawing.Size(160, 41);
+            this.UpdateButton.TabIndex = 9;
+            this.UpdateButton.Text = "Обновить";
+            this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // ReaderName
+            // 
+            this.ReaderName.Text = "ФИО";
+            this.ReaderName.Width = 130;
+            // 
+            // ReaderAdress
+            // 
+            this.ReaderAdress.Text = "Адрес";
+            this.ReaderAdress.Width = 100;
+            // 
+            // ReaderID
+            // 
+            this.ReaderID.Text = "ID";
+            this.ReaderID.Width = 40;
+            // 
+            // ReaderBooks
+            // 
+            this.ReaderBooks.Text = "Книги";
+            this.ReaderBooks.Width = 436;
+            // 
+            // BookName
+            // 
+            this.BookName.Text = "Название";
+            this.BookName.Width = 254;
+            // 
+            // BookAuther
+            // 
+            this.BookAuther.Text = "Автор";
+            this.BookAuther.Width = 104;
+            // 
+            // BookGenre
+            // 
+            this.BookGenre.Text = "Жанр";
+            this.BookGenre.Width = 143;
+            // 
+            // BookIsAvailable
+            // 
+            this.BookIsAvailable.Text = "Наличие";
+            this.BookIsAvailable.Width = 114;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1344, 794);
-            this.Controls.Add(this.button3);
+            this.Controls.Add(this.UpdateButton);
+            this.Controls.Add(this.DeleteBookButton);
+            this.Controls.Add(this.DeleteReaderButton);
             this.Controls.Add(this.checkBox1);
-            this.Controls.Add(this.listView2);
-            this.Controls.Add(this.listView1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BookListView);
+            this.Controls.Add(this.ReaderListView);
+            this.Controls.Add(this.AddBookButton);
+            this.Controls.Add(this.AddReaderButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -116,13 +209,23 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.ListView listView1;
-        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.Button AddReaderButton;
+        private System.Windows.Forms.Button AddBookButton;
+        private System.Windows.Forms.ListView ReaderListView;
+        private System.Windows.Forms.ListView BookListView;
         private System.Windows.Forms.HelpProvider helpProvider1;
         private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button DeleteReaderButton;
+        private System.Windows.Forms.Button DeleteBookButton;
+        private System.Windows.Forms.Button UpdateButton;
+        private System.Windows.Forms.ColumnHeader ReaderName;
+        private System.Windows.Forms.ColumnHeader ReaderAdress;
+        private System.Windows.Forms.ColumnHeader ReaderID;
+        private System.Windows.Forms.ColumnHeader ReaderBooks;
+        private System.Windows.Forms.ColumnHeader BookName;
+        private System.Windows.Forms.ColumnHeader BookAuther;
+        private System.Windows.Forms.ColumnHeader BookGenre;
+        private System.Windows.Forms.ColumnHeader BookIsAvailable;
     }
 }
 
