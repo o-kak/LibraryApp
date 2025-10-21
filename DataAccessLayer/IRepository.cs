5497dd123373a090cc08ed66bde5c42c241fcb92
SunnFloww <butenkovarvara690@gmail.com>
2025-10-21 12:21:36 +0700

    Самери для репозитория

--- DataAccessLayer/IRepository.cs
+++ DataAccessLayer/IRepository.cs
@@ -8,10 +8,37 @@ namespace DataAccessLayer
 {
     public interface IRepository<T>
     {
+        /// <summary>
+        /// Добавляет новую сущность в базу данных.
+        /// </summary>
+        /// <param name="entity">Сущность для добавления.</param>
+        /// <returns>Добавленная сущность (содержит установленный Id).</returns>
         T Add(T entity);
+
+        /// <summary>
+        /// Удаляет сущность из базы данных по её идентификатору.
+        /// </summary>
+        /// <param name="id">Идентификатор сущности для удаления.</param>
         void Delete(int id);
+
+        /// <summary>
+        /// Обновляет существующую сущность в базе данных.
+        /// </summary>
+        /// <param name="entity">Сущность с обновленными данными. EF Core будет отслеживать изменения.</param>
         void Update(T entity);
+
+        /// <summary>
+        /// Читает сущность из базы данных по её идентификатору.
+        /// </summary>
+        /// <param name="id">Идентификатор сущности для чтения.</param>
+        /// <returns>Найденная сущность или null, если сущность с указанным Id не найдена.</returns>
         T ReadById(int id);
+
+        /// <summary>
+        /// Читает все сущности типа T из базы данных.
+        /// </summary>
+        /// <returns>Коллекция всех сущностей типа T.</returns>
         IEnumerable<T> ReadAll();
     }
+
 }
