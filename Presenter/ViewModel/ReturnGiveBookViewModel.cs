f9ad9b46ba201bb5561a02f0c2aadbe2a36432dd
stdash <101164899+stdash@users.noreply.github.com>
2025-12-30 00:34:02 +0700

    вьюмодель для займов

--- Presenter/ViewModel/ReturnGiveBookViewModel.cs
+++ Presenter/ViewModel/ReturnGiveBookViewModel.cs
@@ -1,12 +1,151 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogic;
+using Shared;
+using Model;
+using System.Windows.Input;
 
 namespace Presenter.ViewModel
 {
     public class ReturnGiveBookViewModel : ViewModelBase
     {
+        private readonly ILoan _loanService;
+        private readonly IBookService _bookService;
+        private readonly VMManager _vmManager;
+
+        private ReaderEventArgs _selectedReader;
+        private BookEventArgs _selectedReadersBook;
+        private BookEventArgs _selectedAvailableBook;
+
+        private BindingList<BookEventArgs> _readersBooks;
+        private BindingList<BookEventArgs> _availableBooks;
+
+        public ICommand GiveBookCommand { get; set; }
+        public ICommand ReturnBookCommand { get; set; }
+        public ICommand UpdateCommand { get; set; }
+
+        public ReaderEventArgs SelectedReader
+        {
+            get => _selectedReader;
+            set
+            {
+                if (_selectedReader != value)
+                {
+                    _selectedReader = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public BookEventArgs SelectedReadersBook
+        {
+            get => _selectedReadersBook;
+            set
+            {
+                if (_selectedReadersBook != value)
+                {
+                    _selectedReadersBook = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public BookEventArgs SelectedAvailableBook
+        {
+            get => _selectedAvailableBook;
+            set
+            {
+                if (_selectedAvailableBook != value)
+                {
+                    _selectedAvailableBook = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public BindingList<BookEventArgs> ReadersBooks
+        {
+            get => _readersBooks;
+            set
+            {
+                if (_readersBooks == value)
+                    return;
+                _readersBooks = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public BindingList<BookEventArgs> AvailableBooks
+        {
+            get => _availableBooks;
+            set
+            {
+                if (_availableBooks == value)
+                    return;
+                _availableBooks = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ReturnGiveBookViewModel(VMManager vmManager, ReaderEventArgs reader)
+        {
+            _vmManager = vmManager;
+            SelectedReader = reader;
+        }
+
+        private void LoadReadersBooks()
+        {
+            ReadersBooks.Clear();
+            List<Book> books = _loanService.GetReadersBorrowedBooks(_selectedReader.Id).ToList();
+            foreach (Book book in books)
+            {
+                BookEventArgs args = new BookEventArgs()
+                {
+                    Id = book.Id,
+                    Title = book.Title,
+                    Author = book.Author,
+                    Genre = book.Genre,
+                    IsAvailable = book.IsAvailable,
+                    ReaderId = book.ReaderId,
+                };
+                ReadersBooks.Add(args);
+            }
+        }
+
+        private void LoadAvailableBooks()
+        {
+            AvailableBooks.Clear();
+            List<Book> books = _bookService.GetAvailableBooks().ToList();
+            foreach (Book book in books)
+            {
+                BookEventArgs args = new BookEventArgs()
+                {
+                    Id = book.Id,
+                    Title = book.Title,
+                    Author = book.Author,
+                    Genre = book.Genre,
+                    IsAvailable = book.IsAvailable,
+                    ReaderId = book.ReaderId,
+                };
+                AvailableBooks.Add(args);
+            }
+        }
+
+        private void GiveBook()
+        {
+            _loanService.GiveBook(SelectedAvailableBook.Id, SelectedReader.Id);
+            LoadAvailableBooks();
+            LoadReadersBooks();
+        }
+
+        private void ReturnBook()
+        {
+            _loanService.ReturnBook(SelectedReadersBook.Id, SelectedReader.Id);
+            LoadAvailableBooks();
+            LoadReadersBooks();
+        }
     }
 }
--- Presenter/ViewModel/ServiceLocator.cs
+++ Presenter/ViewModel/ServiceLocator.cs
@@ -25,5 +25,6 @@ namespace Presenter.ViewModel
             LoanService = loanService;
 
         }
+
     }
 }
